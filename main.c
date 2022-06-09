5dea8bbefafcbb12fb45cf92c76bd505762714c2
Henrique Almeida <hcarvalhoalmeida7@gmail.com>
2022-06-09 05:08:01 -0300

    Update main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <time.h>
+main()
+{
+    FILE *pLerCliente = fopen("CLENTES.csv", "r"),
+    *pAdicionarCliente = fopen("CLENTES.csv", "a"),
+    *pLogLocacao = fopen("LOCACAO.csv", "a"),
+    *pLerVeiculo = fopen("VEICULO.csv", "r"),
+    *pAdicionarVeiculo = fopen("VEICULO.csv", "a"),
+    *pAlterarVeiculo = fopen("VEICULO.csv", "w");
+}
