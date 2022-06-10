533e5d42077c90da1e45666787e3bbacd7608dc9
Henrique Almeida <hcarvalhoalmeida7@gmail.com>
2022-06-09 21:33:27 -0300

    Update main.c

--- main.c
+++ main.c
@@ -45,20 +45,20 @@ struct sCliente
 };
 typedef struct sCliente tCliente;
 tCliente cliente[MAX_SIZE_CLIENT];
-struct sCliente inicializaAmigo(char p1[], char p2[], char p3[], char p4[], char p5[], char p6[], char p7[], char p8[], char p9[], char p10[])
+struct sCliente iniCliente(char p1[], char p2[], char p3[], char p4[], char p5[], char p6[], char p7[], char p8[], char p9[], char p10[])
 {
-    tCliente x;
-    strcpy(x.nome, p1);
-    strcpy(x.telefone, p2);
-    strcpy(x.codigo, p3);
-    strcpy(x.endereco.rua, p4);
-    strcpy(x.endereco.numero, p5);
-    strcpy(x.endereco.complemento, p6);
-    strcpy(x.endereco.bairro, p7);
-    strcpy(x.endereco.cidade, p8);
-    strcpy(x.endereco.estado, p9);
-    strcpy(x.endereco.cep, p10);
-    return x;
+    tCliente n;
+    strcpy(n.nome, p1);
+    strcpy(n.telefone, p2);
+    strcpy(n.codigo, p3);
+    strcpy(n.endereco.rua, p4);
+    strcpy(n.endereco.numero, p5);
+    strcpy(n.endereco.complemento, p6);
+    strcpy(n.endereco.bairro, p7);
+    strcpy(n.endereco.cidade, p8);
+    strcpy(n.endereco.estado, p9);
+    strcpy(n.endereco.cep, p10);
+    return n;
 }
 void switchComandos(int p)
 {
@@ -71,6 +71,7 @@ void switchComandos(int p)
 int pesquisarPorCodigo()
 {
     char code[21];
+    fflush(stdin);
     gets(code);
     for (int i = 0; i < MAX_SIZE_CLIENT && strcmp(cliente[i].nome, "EOF") != 0; i++)
         if (strcasecmp(code, cliente[i].codigo) == 0)
@@ -86,7 +87,7 @@ int fimDoArquivo()
     return -1;
 }
 
-void geradorDeCodigo(char codGerado[21])
+void geradorDeCodigo(char codGerado[])
 {
     time_t now = time(NULL);
     struct tm *pt = localtime(&now);
@@ -110,7 +111,7 @@ void addCliente()
 {
     tCliente p;
     system("cls");
-    printf("\n-- Inclusao de um cliente -------\n");
+    printf("\n** Incluir cliente ******-\n");
     printf("\n\nDigite o nome: ");
     scanf(" %[^\n]", &p.nome);
     printf("  cel: ");
@@ -135,12 +136,12 @@ void addCliente()
     int i = fimDoArquivo();
     cliente[i] = p;
     strcpy(cliente[i + 1].nome, "EOF");
-    printf("\nCliente %s incluido.", p.codigo);
+    printf("\n Cliente %s incluido.", p.codigo);
 }
 
 void printCliente(tCliente p)
 {
-    printf("\n Cliente -----------");
+    printf("\n Cliente **********");
     printf("\n Nome:%s", p.nome);
     printf("\n Tel:%s", p.telefone);
     printf("\n Codigo:%s", p.codigo);
@@ -157,49 +158,42 @@ void carregaDadosDoArquivo()
     char p1[150];
     char p2[150];
     int outputFscanf, i = 0;
-    // abre o arquivo
-    // se o arquivo pode ser aberto para leitura
     if (pLerCliente != NULL)
     {
-        // loop de leitura do pLerCliente
         outputFscanf = fscanf(pLerCliente, "%s %[^\n]", &p1, &p2);
         while (outputFscanf != "EOF")
         {
-            // identifica que é um novo registro
             if (strcasecmp(p1, "inicio") == 0)
             {
                 strcpy(contato.nome, "");
                 strcpy(contato.codigo, "");
                 strcpy(contato.telefone, "");
             }
-            // processa cada um dos componente da estrutura
-            else if (strcasecmp(p1, "--nome") == 0)
+            else if (strcasecmp(p1, "**nome") == 0)
                 strcpy(contato.nome, p2);
-            else if (strcasecmp(p1, "--telefone") == 0)
+            else if (strcasecmp(p1, "**telefone") == 0)
                 strcpy(contato.telefone, p2);
-            else if (strcasecmp(p1, "--codigo") == 0)
+            else if (strcasecmp(p1, "**codigo") == 0)
                 strcpy(contato.codigo, p2);
-            else if (strcasecmp(p1, "--rua") == 0)
+            else if (strcasecmp(p1, "**rua") == 0)
                 strcpy(contato.endereco.rua, p2);
-            else if (strcasecmp(p1, "--nro") == 0)
+            else if (strcasecmp(p1, "**nro") == 0)
                 strcpy(contato.endereco.numero, p2);
-            else if (strcasecmp(p1, "--comp") == 0)
+            else if (strcasecmp(p1, "**comp") == 0)
                 strcpy(contato.endereco.complemento, p2);
-            else if (strcasecmp(p1, "--bairro") == 0)
+            else if (strcasecmp(p1, "**bairro") == 0)
                 strcpy(contato.endereco.bairro, p2);
-            else if (strcasecmp(p1, "--cidade") == 0)
+            else if (strcasecmp(p1, "**cidade") == 0)
                 strcpy(contato.endereco.cidade, p2);
-            else if (strcasecmp(p1, "--estado") == 0)
+            else if (strcasecmp(p1, "**estado") == 0)
                 strcpy(contato.endereco.estado, p2);
-            else if (strcasecmp(p1, "--cep") == 0)
+            else if (strcasecmp(p1, "**cep") == 0)
                 strcpy(contato.endereco.cep, p2);
-            // muda a posição do vetor
             else if (strcasecmp(p1, "fim") == 0)
             {
                 cliente[i] = contato;
-                i = i + 1;
+                i++;
             }
-            // lendo o proximo registro dentro da lógica do loop
             outputFscanf = fscanf(pLerCliente, "%s %[^\n]", &p1, &p2);
         }
         strcpy(cliente[i].nome, "EOF");
@@ -207,28 +201,26 @@ void carregaDadosDoArquivo()
     }
 }
 
-void salvaDadosDoArquivo()
+void escreverVetor()
 {
-    // abre o arquivo
     FILE *pSalvarClientes = fopen("CLENTES.csv", "w");
-    int i = 0;
+
     if (pSalvarClientes != NULL)
     {
-        while (strcmp("EOF", cliente[i].nome) != 0)
+        for (int i = 0; strcmp("EOF", cliente[i].nome) != 0; i++)
         {
             fprintf(pSalvarClientes, "inicio\n");
-            fprintf(pSalvarClientes, "--Nome %s\n", cliente[i].nome);
-            fprintf(pSalvarClientes, "--codigo %s\n", cliente[i].codigo);
-            fprintf(pSalvarClientes, "--Telefone %s\n", cliente[i].telefone);
-            fprintf(pSalvarClientes, "--Rua %s\n", cliente[i].endereco.rua);
-            fprintf(pSalvarClientes, "--Nro %s\n", cliente[i].endereco.numero);
-            fprintf(pSalvarClientes, "--Comp %s\n", cliente[i].endereco.complemento);
-            fprintf(pSalvarClientes, "--Bairro %s\n", cliente[i].endereco.bairro);
-            fprintf(pSalvarClientes, "--Cidade %s\n", cliente[i].endereco.cidade);
-            fprintf(pSalvarClientes, "--Estado %s\n", cliente[i].endereco.estado);
-            fprintf(pSalvarClientes, "--Cep %s\n", cliente[i].endereco.cep);
+            fprintf(pSalvarClientes, "**Nome %s\n", cliente[i].nome);
+            fprintf(pSalvarClientes, "**codigo %s\n", cliente[i].codigo);
+            fprintf(pSalvarClientes, "**Telefone %s\n", cliente[i].telefone);
+            fprintf(pSalvarClientes, "**Rua %s\n", cliente[i].endereco.rua);
+            fprintf(pSalvarClientes, "**Nro %s\n", cliente[i].endereco.numero);
+            fprintf(pSalvarClientes, "**Comp %s\n", cliente[i].endereco.complemento);
+            fprintf(pSalvarClientes, "**Bairro %s\n", cliente[i].endereco.bairro);
+            fprintf(pSalvarClientes, "**Cidade %s\n", cliente[i].endereco.cidade);
+            fprintf(pSalvarClientes, "**Estado %s\n", cliente[i].endereco.estado);
+            fprintf(pSalvarClientes, "**Cep %s\n", cliente[i].endereco.cep);
             fprintf(pSalvarClientes, "fim\n");
-            i = i + 1;
         }
         fclose(pSalvarClientes);
     }
@@ -237,13 +229,18 @@ void salvaDadosDoArquivo()
 main()
 {
     srand((unsigned int)(time(NULL)));
-    carregaDadosDoArquivo();
+    cliente[0] = iniCliente("Luquinhas", "31-99999-9999", "17409488241744112022", "rua da luz", "692", "casa", "Santa Lucia", "Belo Horizonte", "MG", "30.110-111");
+    cliente[1] = iniCliente("Ana Claras", "31-99999-9999", "15811391221745382022", "rua da luz", "692", "casa", "Santa Lucia", "Belo Horizonte", "MG", "30.110-112");
+    cliente[2] = iniCliente("Pedro Santos", "31-99999-9999", "08396876981745562022", "rua da luz", "692", "casa", "Santa Lucia", "Belo Horizonte", "MG", "30.110-113");
+    cliente[3] = iniCliente("Theodoro", "31-99999-9999", "98494529411746172022", "rua da luz", "692", "casa", "Santa Lucia", "Belo Horizonte", "MG", "30.110-114");
+    cliente[4] = iniCliente("Joaquim", "31-99999-9999", "13767741661746332022", "rua da luz", "692", "casa", "Santa Lucia", "Belo Horizonte", "MG", "30.110-115");
+    cliente[5] = iniCliente("Henrique", "31-99999-9999", "71221327291746472022", "rua da felicidade", "1000", "101", "Ana lucia", "Belo Horizonte", "MG", "12.345-123");
     printf("sucesso");
     int opcao = 0;
     while (opcao != 9)
     {
         system("cls");
-        printf("\n------- Cadastro de Clientes ---------\n");
+        printf("\n****** Cadastro de Clientes ********\n");
         printf("\n1.Incluir ");
         printf("\n2.Pesquisar por codigo");
         printf("\n9.Encerrar ");
@@ -256,5 +253,5 @@ main()
             system("pause");
         }
     }
-    salvaDadosDoArquivo();
+    escreverVetor();
 }
